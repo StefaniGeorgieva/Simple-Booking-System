33cfa4e60cf977ce06e5c1fa02f5ee1cc56622f3
Stefani Georgieva <stefanigeorgieva@abv.bg>
2020-01-28 22:01:40 +0200

    Removed database and adding functionality

--- database.sql
+++
@@ -1,2 +0,0 @@
-SELECT * FROM booking.table_booking;
-
--- solution.js
+++ solution.js
@@ -1,43 +1,65 @@
-function solution() {
-    let table = document.getElementById('tableNumber').value;
+class Reservation {
+    constructor(tableNum, people, date, time) {
+        this.tableNum = tableNum;
+        this.people = people;
+        this.date = date;
+        this.time = time;
+    }
+}
+
+class Info {
+    static displayInformation() {
+        let info = [];
+
+        let reservation = info;
+        reservation.forEach((e) => Info.addInformation(e));
+    }
+
+    static addInformation(e) {
+        let list = document.getElementById("list");
+        let row = document.createElement('tr');
+
+        row.innerHTML = `
+        <td>${e.tableNum}</td>
+        <td>${e.people}</td>
+        <td>${e.date}</td>
+        <td>${e.time}</td>
+        `;
+
+        list.appendChild(row);
+    }
+
+    static clearField() {
+        document.getElementById('tableNum').value = '';
+        document.getElementById('people').value = '';
+        document.getElementById('date').value = '';
+        document.getElementById('time').value = '';
+    }
+}
+
+//Display info:
+
+document.addEventListener('DOMContentLoaded', Info.displayInformation);
+
+//Add info:
+
+document.getElementById("form").addEventListener('submit', function (ev) {
+
+    ev.preventDefault();
+
+    let table = document.getElementById('tableNum').value;
     let people = document.getElementById('people').value;
     let date = document.getElementById('date').value;
     let time = document.getElementById('time').value;
 
-    let httpr = new XMLHttpRequest();
-    httpr.open("POST", "submit.php", true);
+    if (table === '' || people === '' ||
+        date === '' || time === '') {
 
-    httpr.setRequestHeader('Content-Type', 'application/json');
-    httpr.onreadystatechange = function () {
-        if (httpr.readyState == 4 && httpr.status == 200) {
-            alert('Error');
-        }
+        alert('Please, fill in all fields');
+    } else {
+        let reservation = new Reservation(table, people, date, time);
+
+        Info.addInformation(reservation);
+        Info.clearField();
     }
-
-    httpr.send("table=" + table + "people=" + people + "date=" + date + "time=" + time);
-    
-
-    // //Може да си допуснала грешка
-    // let dataString = 'table=' + table + 'people=' + people + 'date=' + date + 'time=' + time;
-
-
-    // //Checking if all fields are fill
-
-    // if (table == '' || people == '' || date == '' || time == '') {
-    //     alert("Please, fill all fields");
-    // } else {
-    //     $.ajax({
-    //         method: "POST",
-    //         url: "submit.php",
-    //         data: dataString,
-    //         chache: false,
-    //         success: function (html) {
-    //             alert(html);
-    //         }
-    //     });
-    // }
-
-    // return false;
-}
-
-
+});
